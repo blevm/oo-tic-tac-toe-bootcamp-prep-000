921b49eb38967c212c87d6a82f3fc667adec9d2e
Michelle B <mjblevins7@gmail.com>
2018-02-02 02:49:39 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -25,6 +25,11 @@ class TicTacToe
     @board[@index] = @current_player
   end
 
+
+  def position_taken?
+    !(@board[@index].nil? || @board[@index] == " ")
+  end
+  
   WIN_COMBINATIONS = [
     [0,1,2],
     [3,4,5],
