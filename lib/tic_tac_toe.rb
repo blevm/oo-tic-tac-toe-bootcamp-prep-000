1b1114c2fcb4c039fd9b9486299ed5e0b88f3edb
Michelle B <mjblevins7@gmail.com>
2018-02-02 03:03:18 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -7,13 +7,12 @@ class TicTacToe
 
 
   def display_board
-    divider = "-----------"
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts divider
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts divider
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-  end
+   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+   puts "-----------"
+   puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+   puts "-----------"
+   puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+ end
 
   def input_to_index(x)
     x.to_i - 1
