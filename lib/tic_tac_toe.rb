dcbbce6f9ed81c97955ed8625649a148ecb16d20
Michelle B <mjblevins7@gmail.com>
2018-02-02 02:55:54 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -20,9 +20,7 @@ class TicTacToe
   end
 
   def move(board, index, current_player)
-    @index = index
-    @current_player = current_player
-    @board[@index] = @current_player
+    @board[index] = current_player
   end
 
 
